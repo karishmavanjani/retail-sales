f4dd7869cbffc11e25fe4f78574099aed2d53108
Karishma Vanjani <karishmavanjani@Karishmas-MBP.fios-router.home>
2019-04-14 13:02:01 -0400

    weekend work

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $("body").on("keyup keydown keypress change", "input", function (e) {
 	var this_input = $(this),
 		this_row = this_input.closest(".item"),
-		quantity = parseInt(this_row.find(".qty").val()),
-		// cost = parseFloat(this_row.find(".cost").val()),
-		tariff = parseFloat(this_row.data("tariff-percent")),
-		pre_tariff_cost = ((quantity * 18000) / 2500)
-	var total = parseInt(pre_tariff_cost);
+		total_visited = parseFloat(this_row.find(".user_visited").val()),
+		//how to ensure we don't get negative values from user
+		totalstores = parseFloat(this_row.data("stores")),
+		totaljobs = parseFloat(this_row.data("jobs"));
+	console.log("stores", totalstores);
+	console.log("jobs", totaljobs);
+	var total = parseInt((total_visited * totaljobs) / totalstores);
 	// post_tariff_cost = (pre_tariff_cost + (pre_tariff_cost * (tariff / 100)));
 
 	// console.log("Quantity", quantity);
@@ -14,17 +16,13 @@ $("body").on("keyup keydown keypress change", "input", function (e) {
 	// this_row.find(".post_total").find("span").text(post_tariff_cost);
 
 	var pre_total = 0
-	// post_total = 0;
 
 	$(".pre_total").each(function () {
-		//do stuff
 		var this_pre_total = parseFloat($(this).find("span").text());
 		pre_total = pre_total + this_pre_total;
-
-		console.log("Quantity", pre_total);
-
-		$("#pre_total").find("span").text(pre_total);
-		console.log("Quantity", pre_total);
+		// console.log(pre_total);
+		$(".total").find("span").text(pre_total);
+		// console.log("Quantity", pre_total);
 	});
 
 	// $(".post_total").each(function () {
---
+++ retaildata.js
@@ -0,0 +1,19 @@
+let result;
+//take the var data and its
+let data = [
+	{
+		"name": "Payless Shoe Source",
+		"stores": 2500,
+		"jobs": 2,
+  },
+	{
+		"name": "Charlotte Russe",
+		"stores": 10,
+		"jobs": 10,
+  },
+	{
+		"name": "Sears",
+		"stores": 20,
+		"jobs": 1 ",
+  },
+];
