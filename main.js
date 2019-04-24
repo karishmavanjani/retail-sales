59653dd6c567ae57877247a795dd31369165ddb1
Karishma Vanjani <karishmavanjani@Karishmas-MBP.fios-router.home>
2019-04-24 00:28:10 -0400

    sears

--- main.js
+++ main.js
@@ -1,56 +1,47 @@
 $("body").on("keyup keydown keypress change", "input", function (e) {
 
+
 	var this_input = $(this),
 		this_row = this_input.closest(".item"),
 		total_visited = this_row.find(".user_visited").val(),
 		totalstores = parseInt(this_row.data("stores")),
 		totaljobs = parseInt(this_row.data("jobs"));
-	// $(".total").addclass("disabled");
 	if (total_visited === "") {
 		total_visited = 0;
 	}
-
 	total_visited = parseInt(total_visited);
 	// ensuring we take an integer from user irrespective of what he types
-	// console.log("stores", totalstores);
-	// console.log("jobs", totaljobs);
 	if (total_visited < 0 || typeof total_visited !== "number") {
-		// $(".total").find("span").removeclass("disabled");
+		$(".total").removeClass("reset");
 		$(".total").addClass("sticky");
-		$(".total").removeClass("disabled");
 		$(".total").find("span").addClass("error");
 		$(".total").find("span").text("Incorrect values entered");
-		// this_row.find(".pre_total").find("span").addClass("error");
-		// this_row.find(".pre_total").find("span").text("Negative values entered");
 	} else {
+		$(".total").removeClass("reset");
 		$(".total").addClass("sticky");
-		$(".total").removeClass("disabled");
-		// $(".total").find("span").removeclass("disabled");
-		// $(".total").removeClass('disabled');
 		$(".total").find("span").removeClass("error");
-
-		// this_row.find(".pre_total").find("span").removeClass("error");
 		var pre_total = parseInt((total_visited * totaljobs) / totalstores);
 		this_row.find(".pre_total").find("span").text(pre_total);
-
-		// this_row.find(".post_total").find("span").text(post_tariff_cost);
 		console.log(pre_total);
-		// $(".total").find("span").text(pre_total);
-		// var pre_total = 0;
-
-
+		var pre_total = 0;
+		//
 		$(".pre_total").each(function () {
+
+			// for (var i = 0; i < 8; i++) {
+
 			var this_pre_total = $(this).find("span").text();
 
 			if (this_pre_total !== "") {
 				this_pre_total = parseFloat(this_pre_total);
 				pre_total = pre_total + this_pre_total;
-				// console.log(pre_total);
-				// $(".total").find("span").addClass("active");
+				console.log(pre_total);
 				$(".total").find("span").text(pre_total + " Employees Laid-off");
 			}
+
 		});
 	}
+
+
 
 	// $(".post_total").each(function () {
 	// 	//more stuff
@@ -62,10 +53,18 @@ $("body").on("keyup keydown keypress change", "input", function (e) {
 	// $("#post_total").find("span").text(post_total);
 
 });
+$(".resetbutton").click(function () {
+	location.reload();
+	// var pre_total = null;
+	// var this_pre_total = null;
+	// total_visited = null;
+	// totalstores = null;
+	// totaljobs = null;
+	// $(".total").find("span").text(pre_total + " Employees Laid-off");
+	// $(".total").addClass("reset");
+	// console.log(pre_total);
+	// form.reset();
+});
 // $("form").reset(function (event) {
 // 	$(".total").removeClass("sticky");
 // });
-
-$(".resetbutton").click(function () {
-	$(".total").addClass("disabled");
-});
